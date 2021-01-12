d5de16f026061ea0c9692edeb6e3f6d6c280403c
cheny1989 <chenyaa89@gmail.com>
2021-01-12 14:07:44 +0200

    delete request

--- client/src/vacationAdmin/Admin.js
+++ client/src/vacationAdmin/Admin.js
@@ -1,33 +1,49 @@
 import React, { Component } from 'react';
+// import $ from "jquery";
+
 
 class Admin extends Component {
   constructor(props) {
     super(props);
     this.state = {
       vacation: [],
-      id: 0
+      // id: 0
     }
   }
 
-  deleteVacation = () => {
-    alert("DELETE")
-    fetch('/api/apivacation/delete',
-      { method: 'DELETE' });
-  }
+  deleteVacation = (_id) => {
+    const findId = _id;
+    console.log(findId)
+
+    fetch(`/api/apivacation/delete/${_id}`,
+    {
+      method: "DELETE",
+      body: JSON.stringify({ _id: _id }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(function (result) {
+      this.setState({
+          vacation: result
+      });
+      }.bind(this))
+      window.location.reload();
+}
 
   render() {
     const { item } = this.props;
 
     return (
       <div>
-        <div key={item.id} className="vacationListStyle">
+        <div key={item._id} className="vacationListStyle">
           <p className="destination_style">Destination: {item.destination.toUpperCase()}</p>
           <p className="word_wrap">Description: {item.description}</p>
           <p>Price: {item.price}$</p>
           <p>Picture: <br /><img src={item.picture} width="320px" height="180px" alt="img"></img></p>
           <p>Start Date: {item.startDate.split("-").reverse().join("/")}</p>
           <p>End Date: {item.endDate.split("-").reverse().join("/")}</p>
-          <button className="deleteVacationStyle" onClick={() => { this.deleteVacation(item.id) }}>DELETE</button>
+          <button className="deleteVacationStyle" onClick={() => { this.deleteVacation(item._id)}}>DELETE</button>
           <button className="editVacationStyle">EDIT</button>
         </div>
       </div>
--- server/Api/CreateVacation.js
+++ server/Api/CreateVacation.js
@@ -6,7 +6,6 @@ const route = express.Router();
 route.post('/post', async (req, res) => {
     const { destination, description, price, picture, startDate, endDate } = req.body;
     let vacation = {};
-        _id = new mongoose.Types.ObjectId();
         vacation.destination = destination,
         vacation.description = description,
         vacation.price = price,
@@ -34,16 +33,17 @@ route.get('/get', async (req, res) => {
     }
 });
 
-route.delete('/delete', async (req, res) => {
-    try {
-        const deleteVacation = await Vacation.find();
-        Vacation.deleteOne({_id: deleteVacation})
-            res.status(200).json({message: `id${deleteVacation} Deleted`})
-        // res.send(vacation.find(_id => _id._id === deleteVacation))
-    } catch (err) {
-        res.status(400).json({ message: err.message })
-    }
+
+route.delete('/delete/:id', async (req, res) => {
+    let deleteVacation = { _id: req.params.id }
+    Vacation.remove(deleteVacation, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        res.send('Success')
+    });
 })
+
 
 
 module.exports = route;
